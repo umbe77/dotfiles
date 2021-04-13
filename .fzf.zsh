b283336046802a7d121d9f13439862e9145b669f
Roberto Ughi <umbe77@gmail.com>
2021-04-14 00:59:20 +0200

    fzf.zsh with $HOME variable

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/roberto/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/roberto/.fzf/bin"
+if [[ ! "$PATH" == */$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/roberto/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/roberto/.fzf/shell/key-bindings.zsh"
+source "/$HOME/.fzf/shell/key-bindings.zsh"
